4767c2ca4680dc89d807281d1c2bb432ce8c7bc1
marcospmelloo <marcospoliveiramello@gmail.com>
2026-06-05 22:39:26 -0300

    feat: adiciona html

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <nav>
+        <img src={logo} alt="logo Flamengo" className="logo" />
+        <h2>Login</h2>
+      </nav>
+      <main>
+        <div className="Imagem">
+          <img src={flamengo} alt="" className="flamengo" />
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+        <form className="Login">
+          <h1>Login</h1>
+          <p>Já possui cadastro? Acesse abaixo.</p>
+          <p>Caso seja sócio trocedor com FlaID acesse através do FlaID</p>
+          <input type="email" placeholder="Email" className="Email"/>
+          <input type="password" placeholder="Senha" className="Senha"/>
+          <p className="forget">Esqueceu a senha?</p>
+          <button
+            className="Entrar"
+          >
+            Entrar
+          </button>
+          <p className="conta">
+            Não possui uma conta? <a href="">Crie agora</a>
           </p>
+          <button className="FlaID">
+            <p>Entrar com </p>
+            <img src={logo} alt="logo" />
+            <h2 className="FlaIdTexto">Fla-ID</h2>
+          </button>
+        </form>
+      </main>
+      <footer>
+        <img src={logo} alt="" className="logoFooter" />
+        <img src={powered} alt="" className="powered" />
+        <div className="Texto-footer">
+          <div>
+            <p>Termos de uso</p>
+            <p>Aviso de privacidade</p>
+          </div>
+          <div>
+            <p>Perguntas Frequentes</p>
+            <p>Política de Cancelamento</p>
+          </div>
+          <div>
+            <p>Entre ou cadastre-se</p>
+            <p>Compre seu ingresso</p>
+          </div>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
