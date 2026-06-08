ada8bb90ee8a4effcf4a6f9df18d32d2f2aea352
marcospmelloo <marcospoliveiramello@gmail.com>
2026-06-08 15:39:36 -0300

    feat: adiciona styles.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,46 @@
 import { useState } from "react";
-import "./App.css";
+import logo from './assets/logo.png'
+import flamengo from './assets/flamengo.png'
+import powered from './assets/logo-futebolcard-powered-by.png'
+import styles from './styles.module.css';
 
 function App() {
   return (
     <>
       <nav>
-        <img src={logo} alt="logo Flamengo" className="logo" />
+        <img src={logo} alt="logo Flamengo" className={styles.logo} />
         <h2>Login</h2>
       </nav>
       <main>
-        <div className="Imagem">
-          <img src={flamengo} alt="" className="flamengo" />
+        <div className={styles.imagem}>
+          <img src={flamengo} alt="" className={styles.flamengo}/>
         </div>
-        <form className="Login">
+        <form className={styles.login}>
           <h1>Login</h1>
           <p>Já possui cadastro? Acesse abaixo.</p>
           <p>Caso seja sócio trocedor com FlaID acesse através do FlaID</p>
-          <input type="email" placeholder="Email" className="Email"/>
-          <input type="password" placeholder="Senha" className="Senha"/>
-          <p className="forget">Esqueceu a senha?</p>
+          <input type="email" placeholder="Email" className={styles.email}/>
+          <input type="password" placeholder="Senha" className={styles.senha}/>
+          <p className={styles.forget}>Esqueceu a senha?</p>
           <button
-            className="Entrar"
+            className={styles.entrar}
           >
             Entrar
           </button>
-          <p className="conta">
+          <p className={styles.conta}>
             Não possui uma conta? <a href="">Crie agora</a>
           </p>
-          <button className="FlaID">
+          <button className={styles.flaID}>
             <p>Entrar com </p>
             <img src={logo} alt="logo" />
-            <h2 className="FlaIdTexto">Fla-ID</h2>
+            <h2 className={styles.flaIdTexto}>Fla-ID</h2>
           </button>
         </form>
       </main>
       <footer>
-        <img src={logo} alt="" className="logoFooter" />
-        <img src={powered} alt="" className="powered" />
-        <div className="Texto-footer">
+        <img src={logo} alt="" className={styles.logoFooter}/>
+        <img src={powered} alt="" className={styles.powered}/>
+        <div className={styles.textoFooter}>
           <div>
             <p>Termos de uso</p>
             <p>Aviso de privacidade</p>
